568403c7cb52e45c926a12a00078e1583afb8c03
AHMAD ABRAR <ahmad.abrar@mail.ugm.ac.id>
2020-08-19 00:13:53 +0700

    perbaharui tampilan

--- app/src/main/java/com/example/synrgy_codechallenge3/MainActivity.kt
+++ app/src/main/java/com/example/synrgy_codechallenge3/MainActivity.kt
@@ -1,18 +1,142 @@
 package com.example.synrgy_codechallenge3
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.fragment_landing_page1.*
+import android.util.Log
+import com.example.synrgy_codechallenge3.databinding.ActivityMainBinding
+import kotlin.system.exitProcess
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        startActivity(Intent(this, LandingPage1::class.java))
-        this.supportActionBar?.hide()
+
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+
+        val gbk = listOf("batu", "gunting", "kertas")
+
+        val view = binding.root
+        var pilihan: String
+        var pilihanCom: String
+
+        binding.ivBatuPemain.setOnClickListener{
+            pilihan = "batu"
+            Log.d("pilihan user", pilihan)
+            pilihanCom = gbk.random()
+
+            binding.ivBatuPemain.isClickable = false
+            binding.ivGuntingPemain.isClickable = false
+            binding.ivKertasPemain.isClickable = false
+
+            binding.ivBatuPemain.setBackgroundResource(R.drawable.bg_click)
+
+            when (pilihanCom) {
+                "batu" -> {
+                    binding.ivBatuCom.setBackgroundResource(R.drawable.bg_click)
+                    binding.ivHasilPertandingan.setImageResource(R.drawable.draw)
+                    Log.d("hasil pertandingan", "Seimbang")
+                }
+                "gunting" -> {
+                    binding.ivGuntingCom.setBackgroundResource(R.drawable.bg_click)
+                    binding.ivHasilPertandingan.setImageResource(R.drawable.pemain_menang)
+                    Log.d("hasil pertandingan", "Pemain Menang")
+                }
+                "kertas" -> {
+                    binding.ivKertasCom.setBackgroundResource(R.drawable.bg_click)
+                    binding.ivHasilPertandingan.setImageResource(R.drawable.computer_menang)
+                    Log.d("hasil pertandingan", "Computer Menang")
+                }
+            }
+        }
+
+        binding.ivGuntingPemain.setOnClickListener {
+            pilihan = "gunting"
+            Log.d("pilihan user", pilihan)
+
+            pilihanCom = gbk.random()
+
+            binding.ivBatuPemain.isClickable = false
+            binding.ivGuntingPemain.isClickable = false
+            binding.ivKertasPemain.isClickable = false
+
+            binding.ivGuntingPemain.setBackgroundResource(R.drawable.bg_click)
+
+            when (pilihanCom) {
+                "batu" -> {
+                    binding.ivBatuCom.setBackgroundResource(R.drawable.bg_click)
+                    binding.ivHasilPertandingan.setImageResource(R.drawable.computer_menang)
+                    Log.d("hasil pertandingan", "Computer Menang")
+                }
+                "gunting" -> {
+                    binding.ivGuntingCom.setBackgroundResource(R.drawable.bg_click)
+                    binding.ivHasilPertandingan.setImageResource(R.drawable.draw)
+                    Log.d("hasil pertandingan", "Seimbang")
+                }
+                "kertas" -> {
+                    binding.ivKertasCom.setBackgroundResource(R.drawable.bg_click)
+                    binding.ivHasilPertandingan.setImageResource(R.drawable.pemain_menang)
+                    Log.d("hasil pertandingan", "Pemain Menang")
+                }
+            }
+        }
+
+        binding.ivKertasPemain.setOnClickListener {
+            pilihan = "kertas"
+            Log.d("pilihan user", pilihan)
+
+            pilihanCom = gbk.random()
+
+            binding.ivBatuPemain.isClickable = false
+            binding.ivGuntingPemain.isClickable = false
+            binding.ivKertasPemain.isClickable = false
+
+            binding.ivKertasPemain.setBackgroundResource(R.drawable.bg_click)
+
+            when (pilihanCom) {
+                "batu" -> {
+                    binding.ivBatuCom.setBackgroundResource(R.drawable.bg_click)
+                    binding.ivHasilPertandingan.setImageResource(R.drawable.pemain_menang)
+                    Log.d("hasil pertandingan", "Pemain Menang")
+                }
+                "gunting" -> {
+                    binding.ivGuntingCom.setBackgroundResource(R.drawable.bg_click)
+                    binding.ivHasilPertandingan.setImageResource(R.drawable.computer_menang)
+                    Log.d("hasil pertandingan", "Computer Menang")
+                }
+                "kertas" -> {
+                    binding.ivKertasCom.setBackgroundResource(R.drawable.bg_click)
+                    binding.ivHasilPertandingan.setImageResource(R.drawable.draw)
+                    Log.d("hasil pertandingan", "Seimbang")
+                }
+            }
+        }
+
+        binding.ivRestart.setOnClickListener {
+            binding.ivBatuPemain.isClickable = true
+            binding.ivGuntingPemain.isClickable = true
+            binding.ivKertasPemain.isClickable = true
+
+            binding.ivKertasCom.setBackgroundResource(0)
+            binding.ivBatuCom.setBackgroundResource(0)
+            binding.ivGuntingCom.setBackgroundResource(0)
+
+            binding.ivKertasPemain.setBackgroundResource(0)
+            binding.ivBatuPemain.setBackgroundResource(0)
+            binding.ivGuntingPemain.setBackgroundResource(0)
+
+            binding.ivHasilPertandingan.setImageResource(R.drawable.vs)
+        }
+
+        binding.homeBTN.setOnClickListener {
+            finish()
+        }
+
+        binding.closeBTN.setOnClickListener {
+            moveTaskToBack(true)
+            exitProcess(1)
+        }
+
+        setContentView(view)
+
     }
 }
--- app/src/main/java/com/example/synrgy_codechallenge3/MenuActivity.kt
+++ app/src/main/java/com/example/synrgy_codechallenge3/MenuActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import com.example.synrgy_codechallenge3.databinding.ActivityMainBinding
+import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_menu.*
 
 class MenuActivity : AppCompatActivity() {
@@ -13,119 +14,24 @@ class MenuActivity : AppCompatActivity() {
     var inputComp = ""
     var inputPemain1 = ""
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView (binding.root)
+        val bundle = intent.extras
+        val nama = bundle?.getString("nama")
 
-        val listGames = listOf("Batu", "Kertas", "Gunting")
+        tvVsPemain.setText(nama + " VS Pemain")
+        tvVsCom.setText(nama + " VS Komputer")
 
-
-        binding.ivBatu.setOnClickListener{
-            inputPemain1 = "Batu"
-            Log.d("Pilihan Pemain 1", inputPemain1)
-            inputComp = listGames.random()
-
-            binding.ivBatu.isClickable = false
-            binding.ivGunting.isClickable = false
-            binding.ivKertas.isClickable = false
-
-            binding.ivBatu.setBackgroundResource(R.drawable.setonclick_bg)
-
-            when (inputComp) {
-                "Batu" -> {
-                    binding.ivBatuCom.setBackgroundResource(R.drawable.setonclick_bg)
-                    binding.ivVersus.setImageResource(R.drawable.draw)
-                    Log.d("Hasil Suit", "DRAW" )
-                }
-                "Gunting" -> {
-                    binding.ivGuntingCom.setBackgroundResource(R.drawable.setonclick_bg)
-                    binding.ivVersus.setImageResource(R.drawable.pemain1_menang)
-                    Log.d("Hasil Suit", "Pemain1 Menang")
-                }
-                "Kertas" -> {
-                    binding.ivKertasCom.setBackgroundResource(R.drawable.setonclick_bg)
-                    binding.ivVersus.setImageResource(R.drawable.com_menang)
-                    Log.d("Hasil Suit", "COM Menang")
-                }
-            }
+        ivVsPemain.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
         }
 
-        binding.ivGunting.setOnClickListener{
-            inputPemain1 = "Gunting"
-            Log.d("Pilihan Pemain1", inputPemain1)
-            inputComp = listGames.random()
-
-            binding.ivBatu.isClickable = false
-            binding.ivGunting.isClickable = false
-            binding.ivKertas.isClickable = false
-
-            binding.ivGunting.setBackgroundResource(R.drawable.setonclick_bg)
-            when (inputComp) {
-                "Batu" -> {
-                    binding.ivBatuCom.setBackgroundResource(R.drawable.setonclick_bg)
-                    binding.ivVersus.setImageResource(R.drawable.com_menang)
-                    Log.d("Hasil Suit", "COM Menang" )
-                }
-                "Gunting" -> {
-                    binding.ivGuntingCom.setBackgroundResource(R.drawable.setonclick_bg)
-                    binding.ivVersus.setImageResource(R.drawable.draw)
-                    Log.d("Hasil Suit", "DRAW")
-                }
-                "Kertas" -> {
-                    binding.ivKertasCom.setBackgroundResource(R.drawable.setonclick_bg)
-                    binding.ivVersus.setImageResource(R.drawable.pemain1_menang)
-                    Log.d("Hasil Suit", "Pemain1 Menang")
-                }
-            }
-        }
-
-        binding.ivKertas.setOnClickListener {
-            inputPemain1 = "Kertas"
-            Log.d("Pilihan Pemain1", inputPemain1)
-            inputComp = listGames.random()
-
-            binding.ivBatu.isClickable = false
-            binding.ivGunting.isClickable = false
-            binding.ivKertas.isClickable = false
-
-            binding.ivKertas.setBackgroundResource(R.drawable.setonclick_bg)
-            when (inputComp) {
-                "Batu" -> {
-                    binding.ivBatuCom.setBackgroundResource(R.drawable.setonclick_bg)
-                    binding.ivVersus.setImageResource(R.drawable.pemain1_menang)
-                    Log.d("Hasil Suit", "Pemain1 Menang")
-                }
-                "Gunting" -> {
-                    binding.ivGuntingCom.setBackgroundResource(R.drawable.setonclick_bg)
-                    binding.ivVersus.setImageResource(R.drawable.com_menang)
-                    Log.d("Hasil Suit", "COM Menang")
-                }
-                "Kertas" -> {
-                    binding.ivKertasCom.setBackgroundResource(R.drawable.setonclick_bg)
-                    binding.ivVersus.setImageResource(R.drawable.draw)
-                    Log.d("Hasil Suit", "DRAW")
-                }
-            }
-        }
-
-        binding.btnReturn.setOnClickListener{
-            binding.ivBatu.isClickable = true
-            binding.ivGunting.isClickable = true
-            binding.ivKertas.isClickable = true
-
-            binding.ivBatuCom.setBackgroundResource(R.drawable.setonclick_no_bg)
-            binding.ivKertasCom.setBackgroundResource(R.drawable.setonclick_no_bg)
-            binding.ivGuntingCom.setBackgroundResource(R.drawable.setonclick_no_bg)
-
-            binding.ivBatu.setBackgroundResource(R.drawable.setonclick_no_bg)
-            binding.ivKertas.setBackgroundResource(R.drawable.setonclick_no_bg)
-            binding.ivGunting.setBackgroundResource(R.drawable.setonclick_no_bg)
-
-            binding.ivVersus.setImageResource(R.drawable.ic_vs)
+        ivVsCom.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
         }
     }
 }
